19db2fab415c106d10d56f4d1fe124d083b9b64b
Ricardo Calado <jose.calado@pravaler.com.br>
2020-12-01 23:34:38 -0300

    criando  update

---
+++ lib/display.ex
@@ -0,0 +1,32 @@
+defmodule Jogodavelha.Display do
+  use Agent
+
+    def start do
+        initial = %{
+                  row1: [1,2,3],
+                  row2: [4,5,6],
+                  row3: [7,8,9],
+            }
+
+      Agent.start_link(fn -> initial end, name: __MODULE__)
+
+    end
+
+    def value do
+      Agent.get(__MODULE__, & &1)
+    end
+
+
+   def update(key) do
+    Agent.update(__MODULE__,&Map.put(&1,key,[1,3,4]))
+   end
+
+    def display do
+      IO.puts("\n--- Escolha a jogada---\n")
+      [Map.get(value(),:row1),Map.get(value(),:row2),Map.get(value(),:row3)]
+      |> Enum.each(&IO.inspect/1)
+      IO.puts("\n------")
+    end
+
+
+  end
